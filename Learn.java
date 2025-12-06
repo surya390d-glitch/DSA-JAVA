b413a80cd0f298a5fbbb4ba3a1ce64db6cbd5312
surya390d-glitch <surya390d@gmail.com>
2025-12-06 19:59:02 +0530

    third commit

--- Learn.java
+++ Learn.java
@@ -2,6 +2,6 @@ public class Learn {
     public static void main(String [] args) {
         System.out.println(args[0]);
         int a = 10;
-        System.out.println("a");
+        System.out.println("a" + a);
     }
 }
