2ac0a54ba63c2015d6c8b2490bcab8dfe9e12e0a
surya390d-glitch <surya390d@gmail.com>
2025-12-07 18:41:22 +0530

    type conversion and type casting is commited successfully

--- Typeconvcast.java
+++ Typeconvcast.java
@@ -1,12 +1,14 @@
 public class Typeconvcast {
     public static void main(String [] args){
         //int num = 34;
+        /* ***TYPE CONVERSION UH*** */
         float num = 65;/*here the float value is declared as a integer.
         but the output would be like this = 64 converted into 64.0.
         why? because, the JVM consider the larger values(integer < float & double > float...etc.).
         it's happend during the compile time and automatically.
         therefore it is refered as a type conversion.*/
         System.out.println(num);
+        /* ***TYPE CASTING*** */
         int num1 = (int)(34.89f);/*here the float value has been converted into integer value by using type casting.
         this process happend manually.
         output will print like 34.89 into 34.
