a6af673285210e63a78768d5f8cf757880305e29
surya390d-glitch <surya390d@gmail.com>
2025-12-11 13:11:23 +0530

    conditions and loops program is commited successfully

--- Condition_loops.java
+++ Condition_loops.java
@@ -3,10 +3,12 @@ public class Condition_loops {
     public static void main(String []args){
         //If_cond();
         //Forloop();
-        whileloop();
-        dowhile();
-        Max();
-        charprogram();
+        //whileloop();
+        //dowhile();
+        //Max();
+        //charprogram();
+        //fibonacci();
+        duplicate();
     }
     //IF ELSE CONDITION
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,7 +18,7 @@ public class Condition_loops {
        //BODY WHATEVER YOU WANT WRITE  
        } */
  //FOR EXAMPLE:
-/*    public static void If_cond(){
+    public static void If_cond(){
         //IF CONDITION 
         Scanner in = new Scanner(System.in);
         System.out.print("Enter your salry amount: ");
@@ -32,10 +34,10 @@ public class Condition_loops {
         }
         System.out.println("salary status: "+ salary);
         in.close();
-    } */
+    } 
     //FOR LOOP
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//   public static void Forloop(){
+    public static void Forloop(){
 // PROGRAM FOR FORLOOP WITH SYNTAX AND EXAMPLE //
    //SYNTAX:
          //   for(initialization : condition||end : increament) {
@@ -43,7 +45,7 @@ public class Condition_loops {
                // }    
 //FOR EXAMPLE:
 
-/*     for(int i = 0 ; i <= 9 ; i++){
+    for(int i = 0 ; i <= 9 ; i++){
         System.out.print(i+ " ");//output like this : 0123456789 
     }
 
@@ -55,10 +57,10 @@ public class Condition_loops {
                                     here, the initialization part will be '0'and increament 
                                     part will be '2' the outoput should be "ODD" numbers will print.
                                     and, the initialization part will be '1'and increament part 
-                                    will be '2' the outoput should be "EVEN" numbers will print as output.
+                                    will be '2' the outoput should be "EVEN" numbers will print as output.*/
         System.out.print(i + " ");
     }
-    }*/
+    }
 
     //WHILE LOOP
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -148,14 +150,54 @@ public class Condition_loops {
             else {
                 System.out.println("Upper Case.");
             }
+        }
 //another one:
         // System.out.print("Enter your String: ");
         // String str = in.nextLine();
         // System.out.println(str.charAt(2));// here, i'm print the input of index number 1.               
-                                                // ex: input: "hello" ------> output: 'e'.
-
+                                                // ex: input: "hello" ------> output: 'e'.*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//PROGRAM FOR FIBONACCI SERIES: FIND THE Nth FIBONACCI NUMBER.
+            /* theory of fibonacci series:
+            fibonacci series is addition of previous two numbers.
+            for example: 0,1,1,2,3,5,8,13,21....ect. -----> 0+1=1,1+1=2,1+2=3,2+3=5....ect.*/
+    public static void fibonacci(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter your number: ");
+        int num = in.nextInt();
+        int previous_num = 0;
+        int current_num = 1;
+        int count = 2;
+        while(count <= num) {//we already know about first two fibo numbers are "0 & 1". so,the will start from 3||2.
+            int temp = current_num;//let cosider the temprory value is current num.ex: 1
+            current_num = previous_num + current_num;//here, the cuurent value is updated by adding privious + current. ex: 0+1
+            previous_num = temp;//in this line the privious num is ubdated as a temprory value.ex : 0 + 1 = 1.
+            count ++;// count will updated untill the input num is finding.ex: 5
+        }
+        System.out.println(num +"th fibonacci number is: "+current_num);//print tha
+        System.out.println("count: "+count);// count increase untill the while is false
+        in.close();
+    } 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //PROGRAM: find the repeated number in the given input.
+    public static void duplicate(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your target number: ");
+        int target = in.nextInt();
+        int num = 412445144;
+        int count = 0;
+        while(num > 0){
+            int rem = num%10;//GET THE LAST DIGIT IN THIS GIVEN NUMBER. EX: 4 
+        if(rem == target){//check the condition. count the number of " 4's ".
+            count ++;//count the number 
+        }
+        num = num/10;//remove the last digit
+        }
+        System.out.println(target+" is repeated "+count+" times.");//final output: how many "4's" repeat in this number.
     }
-
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//revrse program: 
+    public static void reverse(){
+        
+    }  
 }
