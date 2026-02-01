aeb76c4cf8761e6e8e12128d877ab11102488a17
surya390d-glitch <surya390d@gmail.com>
2026-02-01 22:43:13 +0530

    shadowing commit

--- Shadowing.java
+++ Shadowing.java
@@ -3,18 +3,19 @@ public class Shadowing {
                         //so, we acces the variable from anywhere. this will be shadowed at the line 5.
     public static void main(String [] args){
         System.out.println(num);// 390 will print
-        int num = 890;
+        int num = 7;
         System.out.println(num);// 890 will print. because in the line shadow the num in line 5.
                                 //("Local variable hides a field")
         //shadow();
-        varargs(1,2,"rock","feller");
-        fun(76);
-        fun("velu"); //compiler decide which function that is contain(int,string) 
-
+        // varargs(1,2,"rock","feller");
+        // fun(76);
+        // fun("velu"); //compiler decide which function that is contain(int,string) 
+        boolean ans = Checkprime_or_not(num);
+        System.out.println(ans);
     }
-    public static void shadow(){  
-        System.out.println(num);// 390 will print
-    }
+    // public static void shadow(){  
+    //     System.out.println(num);// 390 will print
+    // }
 //VAERIABLE LENGTH ARGUMENT
     static void varargs(int a, int b,String...y){//variable length argument should will be come the end.     
         // System.out.println(v);
@@ -27,4 +28,23 @@ public class Shadowing {
     static void fun(String name){
         System.out.println(name);
     }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//CHECK PRIME OR NOT
+
+    static boolean Checkprime_or_not(int num){
+        if(num <= 1){
+            return false;
+        }
+        int c = 2;
+        while(c * c <= num){
+            if(num % 2 == 0){
+                return false;
+            }
+            c ++;
+        }
+        return c * c > num; 
+    }
 }
+
