827de4e60a58c26abc30f7240a5e0b73c715d154
surya390d-glitch <surya390d@gmail.com>
2026-02-01 23:32:18 +0530

    code updated

--- Prime.java
+++ Prime.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class Prime{
   public static void main(String args[]){
       Scanner in = new Scanner(System.in);
+      System.out.print("Enter The Number To Check: ");
       int num = in.nextInt();
       boolean ans = check(num);
       System.out.println(ans);
@@ -12,9 +13,10 @@ class Prime{
     }
     int c = 2;
     while(c*c < num){
-      if(num % 2 == 0){
+      if(num % c == 0){
       return false;
       }
+      c++;
     }
     return c*c > num;
   }
