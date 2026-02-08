6cd44997e9b4d79b04b125e918deb4fe1569be22
surya390d-glitch <surya390d@gmail.com>
2026-02-08 16:37:56 +0530

    arrays commited

---
+++ Array.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+class Array {
+    public static void main(String args[]) {
+        int[] arr = { 1, 2, 4, 5, 6, 7, 8, 9 };
+        System.out.println(Arrays.toString(arr));
+    } 
+}
