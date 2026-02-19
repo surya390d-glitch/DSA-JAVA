be3c97aa8c0ce4822029cb65c4ff7800262344ab
surya390d-glitch <surya390d@gmail.com>
2026-02-19 12:43:32 +0530

    apex commited

--- Apex.java
+++ Apex.java
@@ -1,15 +1,21 @@
+
+import java.util.Scanner;
+
 class Apex {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 4, 2, 3, 5, 4, 5 };
         // boolean ans = check(arr);
         // System.out.println(ans);
         // check(arr);
+//----------------------------------------------------------------------------------------------------------------------------------------
         // Student s = new Student("ayon", 56, 78, 90);
         // s.display_Grade();
+//-----------------------------------------------------------------------------------------------------------------------------------------        
         rectangle r1 = new rectangle (12,45);
         r1.display();
-        Bank_Acount ac = new Bank_Account();
-        ac.display_ac();
+//-----------------------------------------------------------------------------------------------------------------------------------------
+        Bank_Account ac = new Bank_Account("Rock Feller",142323,10000);
+        ac.Display_ac();
     }
 
     //----------------------------------------------(non-repeating)----------------------------------------------------------------------------------
@@ -109,18 +115,35 @@ class rectangle {
 //-----------------------------------------------(Bank Account Simulation)------------------------------------------------------------------------
 
 class Bank_Account {
-    int account_num, account_HolderName, Balance;
+    float account_num;
+    double Balance = 0;
+    String account_HolderName;
+    Scanner in = new Scanner(System.in);
 
-    void Deposite() {
-
-    }
-    
-    void Withdraw() {
-
-    }
-
-    void Display_ac() {
-        
+    Bank_Account(String account_HolderName, float account_num, double Balance) {
+        this.account_HolderName = account_HolderName;
+        this.account_num = account_num;
+        this.Balance = Balance;
     }
 
+    double Deposit() {
+            String input = "Enter your deposit amount";
+            System.out.print("Enter your Deposit Ammount: ");
+            double Deposit_amount = in.nextInt();
+            Balance += Deposit_amount;
+            return Balance;
+        }
+
+        double Withdraw() {
+            System.out.print("Enter Your Withdraw Amount: ");
+            double withdraw_amount = in.nextInt();
+            double ubdated_amount =  Balance-withdraw_amount ;
+            return ubdated_amount;
+        }
+
+        void Display_ac() {
+            System.out.println("Deposite Details: " + Deposit());
+            System.out.println("Withdraw_Details: " + Withdraw());
+            // System.out.println("Account_Details: " + Ac_Details());
+        }
 }
