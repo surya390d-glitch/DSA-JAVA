8cad69d031521ee50624a42c5c8976a7250bb012
surya390d-glitch <surya390d@gmail.com>
2026-02-19 11:37:57 +0530

    apexQuiz commited

---
+++ Apex.java
@@ -0,0 +1,126 @@
+class Apex {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 2, 3, 5, 4, 5 };
+        // boolean ans = check(arr);
+        // System.out.println(ans);
+        // check(arr);
+        // Student s = new Student("ayon", 56, 78, 90);
+        // s.display_Grade();
+        rectangle r1 = new rectangle (12,45);
+        r1.display();
+        Bank_Acount ac = new Bank_Account();
+        ac.display_ac();
+    }
+
+    //----------------------------------------------(non-repeating)----------------------------------------------------------------------------------
+    /*1. Find the first non-repeating element
+    Input: {2, 3, 4, 2, 3, 5, 4}*/
+    static void check(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {//2 ------> pick the one element
+            int count = 0;
+            for (int j = 0; j < arr.length; j++) {//2,3,4,2,3,5,4 ------->compares with outer loop
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                System.out.println("ans" + arr[i]);
+            }
+
+        }
+        // return false;
+    }
+}
+//-------------------------------------------------()---------------------------------------------------------------------
+/*2. Student Grade Calculation
+Question:
+Create a class Student with attributes name, marks1, marks2, marks3. Write methods to:
+calculate total
+calculate average
+display grade based on average
+*/
+class Student {
+    String name;
+    int marks1, marks2, marks3;
+
+    Student(String name, int marks1, int marks2, int marks3) {
+        this.name = name;
+        this.marks1 = marks1;
+        this.marks2 = marks2;
+        this.marks3 = marks3;
+    }
+
+    int calculate_Total() {
+        return marks1 + marks2 + marks3;
+    }
+
+    double calc_Avg() {
+        return calculate_Total() / 3.0;
+    }
+
+    void display_Grade() {
+        double avg = calc_Avg();
+        System.out.print("name: " + name);
+        System.out.print("\ntotal marks : " + calculate_Total());
+        System.out.print("\naverage mars:  " + avg);
+
+        if (avg >= 90) {
+            System.out.print("grade A");
+        } else if (avg >= 75) {
+            System.out.print("\ngrade B");
+        } else if (avg >= 50) {
+            System.out.print("\ngrade c");
+        }
+
+        else {
+            System.out.println("\nFail");
+        }
+
+    }
+
+}
+
+//-----------------------------------------(Perimeter)------------------------------------------------------------------------------------------
+
+
+class rectangle {
+    int length;
+    int breadth;
+
+    rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    int calculate_area() {
+        return length * breadth;
+    }
+
+    int calculate_perimeter() {
+        return 2 * (length + breadth);
+    }
+
+    void display() {
+        System.out.println("Area: " + calculate_area() + " m^2");
+        System.out.println("Perimeter: " + calculate_perimeter() + " Cm");
+    }
+}
+
+//-----------------------------------------------(Bank Account Simulation)------------------------------------------------------------------------
+
+class Bank_Account {
+    int account_num, account_HolderName, Balance;
+
+    void Deposite() {
+
+    }
+    
+    void Withdraw() {
+
+    }
+
+    void Display_ac() {
+        
+    }
+
+}
--- Array.java
+++ Array.java
@@ -1,7 +1,64 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
 class Array {
     public static void main(String args[]) {
-        int[] arr = { 1, 2, 4, 5, 6, 7, 8, 9 };
+        int[] arr = { 1, 2, 4, 5, 6, 7, 8, 9 }; // or int[]arr = new int[size]; 
         System.out.println(Arrays.toString(arr));
-    } 
+        // arrayfor(arr1);
+        // int ans = arrayfor(arr1);
+        // System.out.print(Arrays.toString(ans));
+        // String[] str = new String[5];
+        // arrayObj(str);
+        Arr2D();
+    }
+    //-----------------------------[18/02/2006] -----------------------------------------------------------------------------------
+    //-----------------------------(ARRAY OF PRIMITIVES)--------------------------------------------------------------------------
+
+    public static void arrayfor(int arr1[]) {
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = in.nextInt();
+            arr1[2] = 234795;//mutablity(cahnged the original object).
+        }
+        System.out.print(Arrays.toString(arr1));
+
+        // for (int i = 0; i < arr1.length; i++) {
+        //     System.out.print(arr1[i] + " ");
+        // }
+
+        //Enhanced for loop
+        // for (int num : arr1) { //for every element int the array, print the element.
+        // System.out.print(num+" "); //here num represnt every element of the Array.
+        //}
+    }
+
+    //------------------------------(ARRAY OF OBJECTS)------------------------------------------------------------------------------
+    public static void arrayObj(String str[]) {
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+        }
+        System.out.print(Arrays.toString(str));
+        str[2] = "elon"; //orignal String object is changed.
+        System.out.print(Arrays.toString(str));
+    }
+    //-------------------------------(2D ARRAYS)---------------------------------------------------------------------------------
+    /* Strings are immutable in java.------------>>>mutable is nothing but, it means you can change the object.
+       arrays are mutable in java.            */
+
+    public static void Arr2D() {
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new int[3][]; //--------------->first listbox denoted "row" & second one denoted column(it's size doesn,t mantatory to assign)
+        System.out.print(arr.length);
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+            // System.out.println(arr[row][col]+" ");
+        }
+        for (int row = 0; row < arr.length; row++) {
+            System.out.println(arr[row]+" ");
+        }
+    }
 }
