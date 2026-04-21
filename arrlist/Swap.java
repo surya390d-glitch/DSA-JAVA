102cf45b1917f2bdce8fa266e2ac870facb2971d
surya390d-glitch <newbmwjourney1@gmail.com>
2026-04-21 07:03:59 +0530

    21/04/2026

--- arrlist/Swap.java
+++ arrlist/Swap.java
@@ -1,25 +1,38 @@
+import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 56, 7, 8, 99 };
-        // swap(arr, 1, 4);
-        // System.out.println(Arrays.toString(arr));
-        max(arr,5,8);
+        // int[] arr = { 1, 2, 3, 4, 56, 7, 8, 99 };
+    int[] arr = {};
+        swap(arr, 1, 4);
+        System.out.println(Arrays.toString(arr));
+        max(arr, 5, 7);
+        
     }
 
     static void swap(int[] arr, int ind1, int ind2) {
+        if (arr.length == 0) {
+            System.out.println("arr is empty");
+        }
         int temp = arr[ind1];
         arr[ind1] = arr[ind2];
-        arr[ind2] = temp;
-
+        arr[ind2] = temp;  
     }
 
     static void max(int[] arr, int start, int end) {
+
+        if (start < end) {
+            System.out.println("-1");
+        }
+
+        if (arr == null) {
+            System.out.println("-1");
+        }
         int max = arr[start];
         // for (int i : arr) {
         //     if (arr[i] > max) {
         //         max = arr[i];
         //     }
-        for(int i=start ; i<end; i++){
+        for(int i=start ; i<=end; i++){
             if (arr[i] > max) {
                 max = arr[i];
             }
