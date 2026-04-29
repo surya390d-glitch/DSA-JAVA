a1e662a7d9f24a2e49ef913f859f1cbb1e1139a7
surya390d-glitch <newbmwjourney1@gmail.com>
2026-04-29 06:51:53 +0530

    29/04/26

---
+++ linear-search/Search.java
@@ -0,0 +1,27 @@
+//package DSA-JAVA.linear-search;
+import java.util.*;
+class Search {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your target num: ");
+        int target = in.nextInt();
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        // int [] arr = {};
+        // int target = 8;
+        int ans = search(arr,target);
+        System.out.println(ans);
+
+    }
+    
+    static int search(int [] arr,int target){
+        if(arr.length == 0 ){
+            return -1;
+        }
+        for(int i=0; i<=arr.length; i++) {
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }   
+}
