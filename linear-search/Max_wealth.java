78f6074c07b7268db9165035121884c429c336a5
surya390d-glitch <newbmwjourney1@gmail.com>
2026-05-13 07:28:54 +0530

    13/05/2026

---
+++ linear-search/Max_wealth.java
@@ -0,0 +1,24 @@
+public class Max_wealth {
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 3, 5 }, { 2, 4, 1 }, { 7, 2 , 23} };
+        int ans = check_wealth(arr);
+        System.out.println(ans);
+    }
+
+    static int check_wealth(int[][] num) {
+        int ans = Integer.MIN_VALUE;
+        // int sum = 0;
+        for (int per = 0; per < num.length; per++) {
+            int sum = 0;
+            for (int acc = 0; acc < num[per].length; acc++) {
+                sum += num[per][acc];
+            }
+            
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+        //return sum;
+        return ans;
+    }
+}
